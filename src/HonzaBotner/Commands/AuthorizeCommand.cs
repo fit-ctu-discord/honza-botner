d363c4185df146f149b706f719355c16e889aa53
Ondřej Štorc <t-ostorc@microsoft.com>
2020-09-14 22:55:23 +0200

    Authentication basics

---
+++ src/HonzaBotner/Commands/AuthorizeCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.Entities;
+using HonzaBotner.Discord.Command;
+
+namespace HonzaBotner.Commands
+{
+    public class AuthorizeCommand : IChatCommand
+    {
+        private const string LinkTemplate = "https://localhost:5001/Auth?gid={0}&uid={1}";
+
+        public const string CommandText = "authorize";
+
+        public async Task ExecuteAsync(DiscordClient client, DiscordMessage message, CancellationToken cancellationToken = default)
+        {
+            DiscordUser user = message.Author;
+
+            DiscordDmChannel channel = await client.CreateDmAsync(user);
+
+            string link = string.Format(LinkTemplate, message.Channel.GuildId, user.Id);
+
+            await channel.SendMessageAsync($"Hi, authorize by following this link: {link}");
+        }
+    }
+}
--- src/HonzaBotner/Startup.cs
+++ src/HonzaBotner/Startup.cs
@@ -30,7 +30,12 @@ namespace HonzaBotner
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseNpgsql(Configuration["CVUT:ConnectionString"]));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>(options =>
+            {
+                options.SignIn.RequireConfirmedEmail = false;
+                options.SignIn.RequireConfirmedAccount = false;
+                options.SignIn.RequireConfirmedPhoneNumber = false;
+            })
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddAuthentication("CVUT")
                .AddOAuth("CVUT", "CVUT Login", options =>
@@ -49,7 +54,6 @@ namespace HonzaBotner
 
                    var innerHandler = new HttpClientHandler();
                    options.BackchannelHttpHandler = new AuthorizingHandler(innerHandler, options);
-
                    options.Events = new OAuthEvents
                    {
                        OnCreatingTicket = async context =>
@@ -67,18 +71,25 @@ namespace HonzaBotner
                            if (userName == null)
                                throw new InvalidOperationException();
 
-                           context.Identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, userName));
+                           context.Identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, $"{userName}@fit.cvut.cz"));
                            context.Identity.AddClaim(new Claim(ClaimTypes.Email, $"{userName}@fit.cvut.cz")); // HACK: FIX IT
+                           context.Identity.AddClaim(new Claim(ClaimTypes.Name, userName));
+
+                           context.RunClaimActions();
                        }
                    };
                });
-            services.AddRazorPages();
+            services.AddRazorPages(config =>
+            {
+                //config.Conventions.AuthorizePage("Auth");
+            });
 
             services.AddDiscordOptions(Configuration);
             services.AddDiscordBot(config =>
             {
                 // TODO: Commands here
                 config.AddCommand<HiCommand>(HiCommand.ChatCommand);
+                config.AddCommand<AuthorizeCommand>(AuthorizeCommand.CommandText);
             });
         }
 
