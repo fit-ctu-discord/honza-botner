75fea2cf6d39c6a0d2c90d2a879b6cda0a9cd53e
Honza Bittner <git@tenhobi.dev>
2021-02-14 00:56:21 +0100

    Add voice edit command

--- src/HonzaBotner.Discord.Services/Commands/VoiceCommands.cs
+++ src/HonzaBotner.Discord.Services/Commands/VoiceCommands.cs
@@ -1,6 +1,8 @@
 ﻿using System.Threading.Tasks;
+using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
 using HonzaBotner.Discord.Services.Attributes;
 
 namespace HonzaBotner.Discord.Services.Commands
@@ -25,6 +27,30 @@ namespace HonzaBotner.Discord.Services.Commands
             _voiceManager.AddNewVoiceChannelAsync(ctx.Client, ctx.Guild.GetChannel(810277031089930251), ctx.Member,
                 name, limit);
             return ctx.RespondAsync($"I have created new voice channel '{name}' for you!");
+        }
+
+        [Command("edit")]
+        [Description("Create new voice channel. Users has 30 seconds to join.")]
+        public async Task EditVoiceChannel(
+            CommandContext ctx,
+            [Description("Name of the channel.")] string newName,
+            [Description("Limit number of members who can join.")] int? limit = null
+        )
+        {
+            if (ctx.Member.VoiceState.Channel != null && ctx.Member.VoiceState.Channel.Parent.Id == 750055929340231714)
+            {
+                if ((ctx.Member.VoiceState.Channel.PermissionsFor(ctx.Member) & Permissions.ManageChannels) == 0) return;
+
+                await ctx.Member.VoiceState.Channel.ModifyAsync(model =>
+                {
+                    model.Name = newName;
+
+                    if (limit != null)
+                    {
+                        model.Userlimit = limit;
+                    }
+                });
+            }
         }
     }
 }
