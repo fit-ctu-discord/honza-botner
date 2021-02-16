89ee90b5eed96d832dd31a0fab80c4e3525c9938
Honza Bittner <git@tenhobi.dev>
2021-02-16 17:48:47 +0100

    Staff role remove

--- src/HonzaBotner.Discord.Services/Reactions/StaffVerificationReactionHandler.cs
+++ src/HonzaBotner.Discord.Services/Reactions/StaffVerificationReactionHandler.cs
@@ -4,6 +4,7 @@ using DSharpPlus.EventArgs;
 using HonzaBotner.Discord.Services.Options;
 using HonzaBotner.Services.Contract;
 using HonzaBotner.Services.Contract.Dto;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace HonzaBotner.Discord.Services.Reactions
@@ -12,12 +13,18 @@ namespace HonzaBotner.Discord.Services.Reactions
     {
         private readonly IUrlProvider _urlProvider;
         private readonly CommonCommandOptions _config;
+        private readonly IDiscordRoleManager _roleManager;
+        private readonly ILogger<StaffVerificationReactionHandler> _logger;
 
         public StaffVerificationReactionHandler(IUrlProvider urlProvider,
-            IOptions<CommonCommandOptions> options)
+            IOptions<CommonCommandOptions> options,
+            IDiscordRoleManager roleManager,
+            ILogger<StaffVerificationReactionHandler> logger)
         {
             _urlProvider = urlProvider;
             _config = options.Value;
+            _roleManager = roleManager;
+            _logger = logger;
         }
 
         public async Task<IReactionHandler.Result> HandleAddAsync(MessageReactionAddEventArgs eventArgs)
@@ -36,6 +43,22 @@ namespace HonzaBotner.Discord.Services.Reactions
             await channel.SendMessageAsync($"Ahoj, pro získání rolí zaměstnance klikni na: {link}");
 
             return IReactionHandler.Result.Stop;
+        }
+
+        public async Task<IReactionHandler.Result> HandleRemoveAsync(MessageReactionRemoveEventArgs eventArgs)
+        {
+            if (!(eventArgs.Message.Id == _config.VerificationMessageId
+                  && eventArgs.Message.ChannelId == _config.VerificationChannelId))
+                return IReactionHandler.Result.Continue;
+            if (!eventArgs.Emoji.Name.Equals(_config.StaffVerificationEmojiName)) return IReactionHandler.Result.Continue;
+
+            bool ungranted = await _roleManager.UngrantRolesPoolAsync(eventArgs.User.Id, RolesPool.Staff);
+            if (!ungranted)
+            {
+                _logger.LogWarning("Ungranting roles failed.", eventArgs.User.Username, eventArgs.User.Id);
+            }
+
+            return IReactionHandler.Result.Continue;
         }
     }
 }
