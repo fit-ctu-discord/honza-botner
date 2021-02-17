053e17a31572a1c5595e5f90f3d6b9cd1ecf1e79
Honza Bittner <git@tenhobi.dev>
2021-02-17 17:08:44 +0100

    Add pushpin

---
+++ src/HonzaBotner.Discord.Services/Options/PinOptions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace HonzaBotner.Discord.Services.Options
+{
+    public class PinOptions
+    {
+        public static string ConfigName => "PinOptions";
+
+        public int Treshold { get; set; }
+        public Dictionary<string, int> RoleToWeightMapping { get; set; }
+        public string EmojiName { get; set; }
+    }
+}
