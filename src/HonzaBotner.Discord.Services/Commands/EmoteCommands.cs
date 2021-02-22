914c56301680509c22d2c8043e59ddef9a802f91
Ondřej Štorc <storc.1998@gmail.com>
2021-02-22 20:37:02 +0100

    Emotes pagination

--- src/HonzaBotner.Discord.Services/Commands/EmoteCommands.cs
+++ src/HonzaBotner.Discord.Services/Commands/EmoteCommands.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
+using DSharpPlus.Interactivity.Enums;
+using DSharpPlus.Interactivity.Extensions;
+using HonzaBotner.Discord.Extensions;
 using HonzaBotner.Services.Contract;
 using HonzaBotner.Services.Contract.Dto;
 using Microsoft.Extensions.Logging;
@@ -49,92 +52,79 @@ namespace HonzaBotner.Discord.Services.Commands
         private async Task Display<TKey>(CommandContext ctx, bool total, string[] parameters,
             Func<CountedEmoji, TKey> comparer)
         {
-            GetFlags(parameters, out bool animated, out bool nonAnimated, out bool debug);
+            GetFlags(parameters, out bool animated, out bool nonAnimated);
             bool all = animated == nonAnimated;
 
             IEnumerable<CountedEmoji> results = await _emojiCounterService.ListAsync();
             IOrderedEnumerable<CountedEmoji> orderedResults = results.OrderByDescending(comparer);
 
-            Stopwatch stopwatch = new();
-            stopwatch.Start();
-
-            await ctx.RespondAsync("**Statistika používání custom emotes**");
-
             StringBuilder builder = new();
             builder.Append("\n");
 
             int emojisAppended = 0;
-            const int chunkSize = 30;
+            //const int chunkSize = 30;
 
             IReadOnlyDictionary<ulong, DiscordEmoji> emojis = ctx.Guild.Emojis;
 
             foreach (CountedEmoji result in orderedResults)
             {
-                try
+                if (!emojis.TryGetValue(result.Id, out DiscordEmoji? emoji) || emoji == null)
                 {
-                    if (!emojis.TryGetValue(result.Id, out DiscordEmoji? emoji) || emoji == null)
-                    {
-                        continue;
-                    }
-
-                    if (!(emoji.IsAnimated == animated || all))
-                    {
-                        continue;
-                    }
-
-                    string label = total ? "×" : "×/day";
-
-                    builder.Append(emoji)
-                        .Append("`")
-                        .Append(
-                            (total
-                                ? result.Used.ToString()
-                                : $"{result.UsagePerDay:0.00}").PadLeft(10, ' '))
-                        .Append(label)
-                        .Append("`")
-                        .Append(emojisAppended % 3 == 2 ? "\n" : "\t");
-
-                    emojisAppended++;
-
-                    if (emojisAppended == chunkSize)
-                    {
-                        await ctx.RespondAsync(builder.ToString());
-                        builder.Clear();
-                        builder.Append("\n");
-                        emojisAppended = 0;
-                    }
+                    continue;
                 }
-                catch (Exception e)
+
+                if (!(emoji.IsAnimated == animated || all))
                 {
-                    _logger.LogWarning(e, "Couldn't find emote with id {0}", result.Id);
+                    continue;
                 }
+
+                string label = total ? "×" : "×/day";
+
+                builder.Append(emoji)
+                    .Append("`")
+                    .Append(
+                        (total
+                            ? result.Used.ToString()
+                            : $"{result.UsagePerDay:0.00}").PadLeft(10, ' '))
+                    .Append(label)
+                    .Append("`")
+                    .Append(emojisAppended % 3 == 2 ? "\n" : "\t");
+
+                emojisAppended++;
+
+                /*if (emojisAppended == chunkSize)
+                {
+                    await ctx.RespondAsync(builder.ToString());
+                    builder.Clear();
+                    builder.Append("\n");
+                    emojisAppended = 0;
+                }*/
             }
 
-            string remaining = builder.ToString();
-            if (!string.IsNullOrEmpty(remaining.Trim()))
+            if (emojisAppended > 0)
             {
-                await ctx.RespondAsync(builder.ToString());
-            }
-
-            builder.Clear();
-
-            stopwatch.Stop();
-            if (debug)
-            {
-                await ctx.RespondAsync($"Execution of emoji print took: {stopwatch.Elapsed}");
+                var interactivity = ctx.Client.GetInteractivity();
+                DiscordEmbedBuilder embedBuilder = new()
+                {
+                    Author = new DiscordEmbedBuilder.EmbedAuthor
+                    {
+                        IconUrl = ctx.Member.AvatarUrl, Name = ctx.Member.RatherNicknameThanUsername()
+                    },
+                    Title = "Statistika používání custom emotes",
+                };
+                var pages = interactivity.GeneratePagesInEmbed(builder.ToString(),
+                    SplitType.Line, embedBuilder);
+                await ctx.Channel.SendPaginatedMessageAsync(ctx.Member, pages);
             }
         }
 
-        private void GetFlags(string[] parameters, out bool animated, out bool nonAnimated, out bool debug)
+        private void GetFlags(string[] parameters, out bool animated, out bool nonAnimated)
         {
-            debug = animated = nonAnimated = false;
+            animated = nonAnimated = false;
             foreach (string parameter in parameters)
             {
                 switch (parameter)
                 {
-                    case "debug":
-                        debug = true;
-                        break;
                     case "animated":
                         animated = true;
                         break;
