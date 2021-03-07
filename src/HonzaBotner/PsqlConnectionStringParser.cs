3c61198682a5d2feec181fa74fa7969dc1f3c781
Ondřej Štorc <storc.1998@gmail.com>
2021-03-08 00:51:32 +0100

    Connection string fix

--- src/HonzaBotner/PsqlConnectionStringParser.cs
+++ src/HonzaBotner/PsqlConnectionStringParser.cs
@@ -10,7 +10,7 @@ namespace HonzaBotner
             if (isUrl && url != null)
             {
                 return
-                    $"host={url.Host};username={url.UserInfo.Split(':')[0]};password={url.UserInfo.Split(':')[1]};database={url.LocalPath.Substring(1)};pooling=true;";
+                    $"host={url.Host};username={url.UserInfo.Split(':')[0]};password={url.UserInfo.Split(':')[1]};database={url.LocalPath.Substring(1)};pooling=true;SSL Mode=Require";
             }
 
             return connectionUrl;
