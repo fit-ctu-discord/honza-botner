dbd4e523926a125dec06bec4dc9d83e8193a6018
Honza Bittner <git@tenhobi.dev>
2021-03-10 22:40:22 +0100

    Add info command

--- src/HonzaBotner.Discord.Services/ServiceCollectionExtensions.cs
+++ src/HonzaBotner.Discord.Services/ServiceCollectionExtensions.cs
@@ -12,6 +12,7 @@ namespace HonzaBotner.Discord.Services
             serviceCollection.Configure<CommonCommandOptions>(configuration.GetSection(CommonCommandOptions.ConfigName));
             serviceCollection.Configure<CustomVoiceOptions>(configuration.GetSection(CustomVoiceOptions.ConfigName));
             serviceCollection.Configure<PinOptions>(configuration.GetSection(PinOptions.ConfigName));
+            serviceCollection.Configure<InfoOptions>(configuration.GetSection(InfoOptions.ConfigName));
 
             return serviceCollection;
         }
