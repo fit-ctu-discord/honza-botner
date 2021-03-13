644492807bdb118b6d5f345a6d99a6fa4c43ea0f
Honza Bittner <git@tenhobi.dev>
2021-03-13 15:22:34 +0100

    Fix warnings

--- src/HonzaBotner.Discord.Services/EventHandlers/ServerBoosterHandler.cs
+++ src/HonzaBotner.Discord.Services/EventHandlers/ServerBoosterHandler.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using DSharpPlus;
 using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
 using HonzaBotner.Discord.EventHandler;
