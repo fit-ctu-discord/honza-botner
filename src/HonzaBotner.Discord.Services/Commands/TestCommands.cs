2c09cd651dbbc9f395dfab8305074fb1cde7b312
Jan Bittner <git@tenhobi.dev>
2021-03-24 19:15:27 +0100

    Test boost count command

---
+++ src/HonzaBotner.Discord.Services/Commands/TestCommands.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using HonzaBotner.Discord.Services.Attributes;
+
+namespace HonzaBotner.Discord.Services.Commands
+{
+    [Group("test")]
+    [Description("Testing commands")]
+    [RequireMod]
+    public class TestCommands : BaseCommandModule
+    {
+        [Command("boostCount")]
+        public async Task BoostCount(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync(ctx.Guild.PremiumSubscriptionCount.ToString());
+        }
+    }
+}
