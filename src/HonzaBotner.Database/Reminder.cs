7034717e3cf6559f9ee1aff01653eca084d9cc27
Jiří Vrba <jiri@vrba.dev>
2021-07-30 12:44:13 +0200

    Update Reminder primary key definition

--- src/HonzaBotner.Database/Reminder.cs
+++ src/HonzaBotner.Database/Reminder.cs
@@ -1,12 +1,14 @@
 #nullable enable
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HonzaBotner.Database
 {
     public class Reminder
     {
-        [Key] public ulong Id { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
 
         // Mapped Discord ID of the user that created this reminder
         public ulong OwnerId { get; set; }
