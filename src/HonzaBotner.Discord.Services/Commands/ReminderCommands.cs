f205005482f38def33ffeb8f1a69863c364f6bf6
Jiří Vrba <jiri@vrba.dev>
2021-07-30 11:34:23 +0200

    Add date time parsing to reminder command executor

--- src/HonzaBotner.Discord.Services/Commands/ReminderCommands.cs
+++ src/HonzaBotner.Discord.Services/Commands/ReminderCommands.cs
@@ -1,7 +1,13 @@
+using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Chronic.Core;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
+using HonzaBotner.Discord.Extensions;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HonzaBotner.Discord.Services.Commands
 {
@@ -16,12 +22,37 @@ namespace HonzaBotner.Discord.Services.Commands
         public async Task Create(
             CommandContext context,
             [Description("Title of the reminder.")] string title,
-            [Description("Date or time of the reminder")] string date,
+            [Description("Date or time of the reminder")] string rawDatetime,
             [Description("Optional additional content"), RemainingText] string content
         )
         {
-            var emoji = DiscordEmoji.FromName(context.Client, ":ok_hand:");
-            await context.Message.CreateReactionAsync(emoji);
+            var datetime = ParseDateTime(rawDatetime);
+
+            if (datetime == null)
+            {
+                await context.RespondErrorAsync(
+                    $"Cannot parse datetime string `{rawDatetime}`",
+                    "Try using an explicit datetime or expressions like `in 30 minutes`, `tomorrow at 8:00`, ..."
+                );
+                return;
+            }
+
+            await context.RespondAsync(datetime.ToString());
+        }
+
+        private static DateTime? ParseDateTime(string datetime)
+        {
+            // First try to parse the explicit datetime formats
+            // Cases with time only are handled by the parser
+            string[] formats = { "dd. MM. yyyy HH:mm", "dd.MM.yyyy HH:mm", "dd. MM. yyyy", "dd.MM.yyyy" };
+
+            if (DateTime.TryParseExact(datetime, formats, CultureInfo.InvariantCulture, DateTimeStyles.AssumeLocal,
+                out DateTime parsed))
+            {
+                return parsed;
+            }
+
+           return new Parser().Parse(datetime)?.Start;
         }
     }
 }
