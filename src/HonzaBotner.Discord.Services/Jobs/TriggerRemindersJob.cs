51b91819cf21a0610d994820f67d58d0d6d4f591
Jiří Vrba <jiri@vrba.dev>
2021-07-30 17:26:22 +0200

    Reminders trigger implemented

--- src/HonzaBotner.Discord.Services/Jobs/TriggerRemindersJob.cs
+++ src/HonzaBotner.Discord.Services/Jobs/TriggerRemindersJob.cs
@@ -1,6 +1,14 @@
+using System;
+using System.Linq;
 using System.Threading.Tasks;
+using Chronic.Core.System;
+using DSharpPlus.Entities;
 using HonzaBotner.Database;
+using HonzaBotner.Discord.Services.EventHandlers;
+using HonzaBotner.Discord.Services.Options;
 using HonzaBotner.Services.Contract;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace HonzaBotner.Discord.Services.Jobs
 {
@@ -8,12 +16,22 @@ namespace HonzaBotner.Discord.Services.Jobs
     {
         private readonly IRemindersService _service;
 
-        private readonly DiscordWrapper _guild;
+        private readonly ILogger<TriggerRemindersJob> _logger;
 
-        public TriggerRemindersJob(IRemindersService service, DiscordWrapper guild)
+        private readonly ReminderOptions _options;
+
+        private readonly DiscordWrapper _discord;
+
+        public TriggerRemindersJob(
+            IRemindersService service,
+            ILogger<TriggerRemindersJob> logger,
+            IOptions<ReminderOptions> options,
+            DiscordWrapper discord)
         {
             _service = service;
-            _guild = guild;
+            _logger = logger;
+            _options = options.Value;
+            _discord = discord;
         }
 
         public async Task Dispatch()
@@ -25,8 +43,34 @@ namespace HonzaBotner.Discord.Services.Jobs
 
         private async void SendReminderNotification(Reminder reminder)
         {
-            var channel = await _guild.Client.GetChannelAsync(0);
-            var message = await channel.GetMessageAsync(reminder.MessageId);
+            try
+            {
+                var channel = await _discord.Client.GetChannelAsync(reminder.ChannelId);
+                var message = await channel.GetMessageAsync(reminder.MessageId);
+                var emoji = DiscordEmoji.FromUnicode(_options.JoinEmojiName);
+
+                var receivers = await message.GetReactionsAsync(emoji, 100);
+                var mentions = string.Join(", ", receivers.Select(user => user.Mention) + $"<@{reminder.OwnerId}>");
+
+                await channel.SendMessageAsync(mentions, embed: CreateReminderEmbed(reminder));
+                await message.ModifyAsync("Reminder expired");
+                await message.DeleteAllReactionsAsync();
+                await _service.CancelReminderAsync(reminder);
+            }
+            catch (Exception exception)
+            {
+                _logger.LogCritical("Exception during reminder trigger", exception);
+            }
+        }
+
+        private static DiscordEmbed CreateReminderEmbed(Reminder reminder)
+        {
+            return new DiscordEmbedBuilder()
+                .WithTitle("🔔 " + reminder.Title)
+                .WithDescription(reminder.Content ?? "")
+                .WithColor(new DiscordColor("#ffac33"))
+                .WithTimestamp(reminder.DateTime)
+                .Build();
         }
     }
 }
