792dfd6af5d557509b007af3f52475d6dd13580a
Jiří Vrba <jiri@vrba.dev>
2021-07-30 14:47:07 +0200

    Formatting

--- src/HonzaBotner.Services.Contract/IRemindersService.cs
+++ src/HonzaBotner.Services.Contract/IRemindersService.cs
@@ -7,6 +7,7 @@ namespace HonzaBotner.Services.Contract
     public interface IRemindersService
     {
         public Task<Reminder> CreateReminderAsync(ulong ownerId, ulong messageId, DateTime datetime, string title, string? content);
+
         public Task CancelReminderAsync(Reminder reminder);
 
         public Task<Reminder?> GetByMessageIdAsync(ulong messageId);
