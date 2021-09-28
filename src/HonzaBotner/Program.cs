e8e240ce9badc7cbfe65b648d115e5c3507292aa
stepech <st3pech@protonmail.com>
2021-09-28 15:43:46 +0200

    Remove space

--- src/HonzaBotner/Program.cs
+++ src/HonzaBotner/Program.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration; 
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace HonzaBotner
