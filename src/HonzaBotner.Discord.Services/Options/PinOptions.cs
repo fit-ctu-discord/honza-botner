4fcaaa5a63bbcba49203510554ae2b807f742456
stepech <29132060+stepech@users.noreply.github.com>
2022-04-13 23:29:50 +0200

    Reformat: Treshold -> Threshold

--- src/HonzaBotner.Discord.Services/Options/PinOptions.cs
+++ src/HonzaBotner.Discord.Services/Options/PinOptions.cs
@@ -7,7 +7,7 @@ public class PinOptions
 {
     public static string ConfigName => "PinOptions";
 
-    public int Treshold { get; set; }
+    public int Threshold { get; set; }
     public Dictionary<string, int> RoleToWeightMapping { get; set; }
     public string TemporaryPinName { get; set; }
     public string PermanentPinName { get; set; }
