9479a7cf2cac3db6aad148f57f9c6ccc71400889
Antonín Kříž <15214494+antoninkriz@users.noreply.github.com>
2022-04-16 17:03:45 +0200

    Made Sha256HashService even faster
    I benchmarked all 3 versions and this one is actually even faster than I thought.

--- src/HonzaBotner.Services/Sha256HashService.cs
+++ src/HonzaBotner.Services/Sha256HashService.cs
@@ -6,17 +6,20 @@ namespace HonzaBotner.Services;
 
 public class Sha256HashService : IHashService
 {
+    private static readonly char[] HexAlphabet = new char[] {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
     public string Hash(string input)
     {
         byte[] toBeHashed = Encoding.UTF8.GetBytes(input);
         byte[] bytes = SHA256.HashData(toBeHashed);
 
-        StringBuilder strB = new(64);
-        foreach (byte b in bytes)
+        var c = new char[bytes.Length * 2];
+        for (int i = 0, j = 0; i < bytes.Length; ++i)
         {
-            strB.Append(b.ToString("x2"));
+            c[j] = HexAlphabet[bytes[i] >> 4];
+            c[++j] = HexAlphabet[bytes[i] & 0xF];
         }
 
-        return strB.ToString();
+        return new string(c);
     }
 }
