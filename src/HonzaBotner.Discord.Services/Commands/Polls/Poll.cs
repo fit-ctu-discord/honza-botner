3a68717e6d38730047ec479fc37a56bbdaa5cdcf
stepech <29132060+stepech@users.noreply.github.com>
2022-05-02 00:45:30 +0200

    poll: Rough fix for reaction's disappearing

--- src/HonzaBotner.Discord.Services/Commands/Polls/Poll.cs
+++ src/HonzaBotner.Discord.Services/Commands/Polls/Poll.cs
@@ -10,41 +10,37 @@ namespace HonzaBotner.Discord.Services.Commands.Polls;
 
 public abstract class Poll
 {
-    public abstract List<string> OptionsEmoji { get; }
+    protected abstract List<string> OptionsEmoji { get; }
     public abstract string PollType { get; }
-
-    public virtual List<string> ActiveEmojis
+    protected virtual List<string> UsedEmojis
     {
-        get => OptionsEmoji.GetRange(0, _choices.Count);
+        get => OptionsEmoji.GetRange(0, NewChoices.Count);
     }
 
+    protected List<string> NewChoices;
+
     public readonly string AuthorMention;
-    private readonly List<string> _choices;
-    private readonly DiscordMessage? _existingPollMessage;
-    private readonly string _question;
+    protected readonly DiscordMessage? ExistingPollMessage;
+    protected readonly string Question;
 
     protected Poll(string authorMention, string question, List<string>? options = null)
     {
         AuthorMention = authorMention;
-        _question = question;
-        _choices = options ?? new List<string>();
+        Question = question;
+        NewChoices = options ?? new List<string>();
     }
 
     protected Poll(DiscordMessage originalMessage)
     {
-        _existingPollMessage = originalMessage;
-        DiscordEmbed originalPoll = _existingPollMessage.Embeds[0];
+        ExistingPollMessage = originalMessage;
+        DiscordEmbed originalPoll = ExistingPollMessage.Embeds[0];
 
         // Extract original author Mention via discord's mention format <@!123456789>.
         AuthorMention = originalPoll.Description.Substring(
             originalPoll.Description.LastIndexOf("<", StringComparison.Ordinal)
         );
 
-        _choices = originalPoll.Fields?
-            .Select(ef => ef.Value)
-            .ToList() ?? new List<string>();
-
-        _question = originalPoll.Title;
+        Question = originalPoll.Title;
     }
 
     public async Task PostAsync(DiscordClient client, DiscordChannel channel)
@@ -54,23 +50,9 @@ public abstract class Poll
         Task _ = Task.Run(async () => { await AddReactionsAsync(client, pollMessage); });
     }
 
-    public virtual async Task AddOptionsAsync(DiscordClient client, IEnumerable<string> newOptions)
+    protected async Task AddReactionsAsync(DiscordClient client, DiscordMessage message, List<string>? reactions = null)
     {
-        if (_existingPollMessage == null)
-        {
-            throw new InvalidOperationException("You can edit only poll constructed from sent message.");
-        }
-
-        _choices.AddRange(newOptions);
-
-        await _existingPollMessage.ModifyAsync(Build(client, _existingPollMessage.Channel.Guild));
-
-        Task _ = Task.Run(async () => { await AddReactionsAsync(client, _existingPollMessage); });
-    }
-
-    protected async Task AddReactionsAsync(DiscordClient client, DiscordMessage message)
-    {
-        foreach (string reaction in ActiveEmojis)
+        foreach (string reaction in reactions ?? UsedEmojis)
         {
             await message.CreateReactionAsync(DiscordEmoji.FromName(client, reaction));
         }
@@ -78,18 +60,18 @@ public abstract class Poll
 
     private DiscordEmbed Build(DiscordClient client, DiscordGuild guild)
     {
-        if (_choices.Count > OptionsEmoji.Count)
+        if (NewChoices.Count > OptionsEmoji.Count)
         {
             throw new ArgumentException($"Too many options. Maximum options is {OptionsEmoji.Count}.");
         }
 
         DiscordEmbedBuilder builder = new()
         {
-            Title = _question.RemoveDiscordMentions(guild),
+            Title = Question.RemoveDiscordMentions(guild),
             Description = "By: " + AuthorMention // Author needs to stay as the last argument
         };
 
-        _choices.Zip(ActiveEmojis).ToList().ForEach(pair =>
+        NewChoices.Zip(UsedEmojis).ToList().ForEach(pair =>
         {
             (string? answer, string? emojiName) = pair;
 
