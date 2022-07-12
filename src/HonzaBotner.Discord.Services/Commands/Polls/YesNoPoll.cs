ca3d95aca179b2163456630f9ea517a71fc917bd
stepech <29132060+stepech@users.noreply.github.com>
2022-07-12 22:41:07 +0100

    Revert "polls: Cleanup imports"
    This reverts commit 1272437ef2367af4f91ae91fc3da8121a770d6f0.

--- src/HonzaBotner.Discord.Services/Commands/Polls/YesNoPoll.cs
+++ src/HonzaBotner.Discord.Services/Commands/Polls/YesNoPoll.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DSharpPlus;
 using DSharpPlus.Entities;
 
 namespace HonzaBotner.Discord.Services.Commands.Polls;
